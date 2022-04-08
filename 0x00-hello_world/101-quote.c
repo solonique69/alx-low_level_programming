6e333504085642ba20272340d5442eb045b3eaa3
Leon <solonique2020@gmail.com>
2022-04-07 22:36:01 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
-/*
-
-   * main - prints to string
-
-    * Description: Prints "and that piece of art is useful.." without puts
-
-     * Return: 1
-
-      */
+/**
+	* main - prints to string
+	* Description: Prints "and that piece of art is useful.." without puts
+	* Return: 1
+*/
 
 int main(void)
 
