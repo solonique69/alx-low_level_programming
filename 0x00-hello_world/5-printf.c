2b60dc4155b99a4139e7417317f49edb7b49914a
Leon <solonique2020@gmail.com>
2022-04-07 21:52:41 -0700

    Print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/**
+/*
    *main - Entry point
     *Return: Always 0 (Success)
-     */
+*/
 
 
 
@@ -11,7 +11,7 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
 
