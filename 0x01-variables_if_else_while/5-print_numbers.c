db9b8a1b53edf4be94e7d30ec6912f40b637e6e2
Solomon Okomowho <solonique2020@gmail.com>
2023-10-07 00:06:36 +0100

    Task 5 - Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description; A C-program that counts single digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int FirsTen;
+
+	for (FirsTen = 0; FirsTen < 10; FirsTen++)
+	{
+		printf("%d", FirsTen);
+	}
+
+	printf("\n");
+
+	return (0);
+}
