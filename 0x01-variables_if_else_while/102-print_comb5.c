e5ce4a2fee624951cc02ad8d760b82bf14cc6cf6
Solomon Okomowho <solonique2020@gmail.com>
2023-10-07 23:53:45 +0100

    Task 12 - Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,11 @@
 
 int main(void)
 {
-	for (int num1 = 0; num1 <= 99; num1++)
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (int num2 = num1; num2 <= 99; num2++)
+		for (num2 = num1; num2 <= 99; num2++)
 		{
 			if (num1 != num2)
 			{
