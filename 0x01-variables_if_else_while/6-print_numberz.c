5e9fb196c063009a837f0f7c113e0ea10216a4dd
Solomon Okomowho <solonique2020@gmail.com>
2023-10-07 10:14:23 +0100

    Task 6 - Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description; A C-program that counts single digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
