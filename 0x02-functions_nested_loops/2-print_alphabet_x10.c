d295da11e8c0ee6ef6f22195fa703ce22ef3c10d
Solomon Okomowho <solonique2020@gmail.com>
2023-10-10 18:03:33 +0100

    Task 2 - 10 x alphabet1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Description; A C-function that outputs lower case letters
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char Xn;
+	int Jade_i;
+
+	while (Jade_i < 10)
+	{
+		for (Xn = 'a'; Xn <= 'z'; Xn++)
+		{
+			_putchar(Xn);
+		}
+
+		Jade_i++
+		_putchar('\n');
+	}
+}
