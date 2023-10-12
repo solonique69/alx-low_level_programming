d634b9fb79fb4917850092333f3f842332642772
Solomon Okomowho <solonique2020@gmail.com>
2023-10-13 00:48:46 +0100

    task 3 - The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - Printsnumbers
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		-putchar('i');
+		i++;
+	}
+
+	-putchar('\n');
+}
