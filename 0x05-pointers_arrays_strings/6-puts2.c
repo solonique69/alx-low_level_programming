315a2eab507dcf2738bad11d0ffeb10d47a6602d
Solomon Okomowho <solonique2020@gmail.com>
2023-10-18 14:12:44 +0100

    Task 6 -  Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Print every other character.
+ * @str: The input string.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
+
