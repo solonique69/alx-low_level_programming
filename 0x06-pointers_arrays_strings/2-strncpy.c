dcef288de1cede6203a64fff270817ea2efceae1
Solomon Okomowho <solonique2020@gmail.com>
2023-10-18 19:25:26 +0100

    Task 0 -  strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy a string
+ * @dest: Destination buffer
+ * @src: Source string
+ * @n: Maximum number of bytes to copy from src
+ * Return: A pointer to the destination buffer dest
+ */
+
+char *_strncpy(char *dest, const char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
