7ac05e5936ffd72894f29399f4186d0f15ed86f0
Solomon Okomowho <solonique2020@gmail.com>
2023-10-19 00:03:55 +0100

    Task 11 - It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,55 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * infinite_add - adds two numbers
- * @n1: number one.
- * @n2: number two.
- * @r: buffer that the function will use to store the result.
- * @size_r: buffer size:
- * Return: the pointer to dest.
+ * infinite_add - Add two numbers
+ * @n1: First number as a string
+ * @n2: Second number as a string
+ * @r: Buffer to store the result
+ * @size_r: Size of the buffer
+ * Return: Pointer to the result, or 0 if the result cannot be stored
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int carry = 0;
+	int i, j, k;
+	int sum, num1, num2;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
+	int len1 = _strlen(n1);
+	int len2 = _strlen(n2);
+
+	if (size_r <= len1 || size_r <= len2)
 		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+
+	for (i = 0; i < size_r; i++)
+		r[i] = '\0';
+
+	i = len1 - 1;
+	j = len2 - 1;
+	k = size_r - 2;
+
+	while (i >= 0 || j >= 0 || carry > 0)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-			*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-			bg--, size_r--;
+		num1 = (i >= 0) ? n1[i] - '0' : 0;
+		num2 = (j >= 0) ? n2[j] - '0' : 0;
+		sum = num1 + num2 + carry;
+		carry = sum / 10;
+		sum = sum % 10;
+		r[k] = sum + '0';
+		i--;
+		j--;
+		k--;
 	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+
+	return (r + k + 1);
 }
