fd7ae3eeb0404bd59f180fc88153d7bebc4e6930
Solomon Okomowho <solonique2020@gmail.com>
2023-10-18 19:43:03 +0100

    Task 0 -  strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,22 +8,15 @@
  * Return: A pointer to the destination buffer dest
  */
 
-char *_strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	/*i = 0;*/
-
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
 
-	while (i < n)
-	{
+	for (; n > i; i++)
 		dest[i] = '\0';
-		i++;
-	}
 
 	return (dest);
 }
