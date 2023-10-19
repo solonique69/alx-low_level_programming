ebbdd491462a514a078bfc39b1a37f92c102999a
Solomon Okomowho <solonique2020@gmail.com>
2023-10-19 10:32:51 +0100

    Task 11 - It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,37 +1,42 @@
 #include "main.h"
 
 /**
- * infinite_add - Add two numbers
+ * infinite_add - Adds two numbers
  * @n1: First number as a string
  * @n2: Second number as a string
  * @r: Buffer to store the result
  * @size_r: Size of the buffer
  * Return: Pointer to the result, or 0 if the result cannot be stored
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0;
 	int i = _strlen(n1) - 1;
 	int j = _strlen(n2) - 1;
 	int k = size_r - 2;
 
 	r[size_r - 1] = '\0';
 
+	int carry = 0;
+
 	while (i >= 0 || j >= 0 || carry)
 	{
-		int num1 = (i >= 0) ? n1[i] - '0' : 0;
-		int num2 = (j >= 0) ? n2[j] - '0' : 0;
-		int sum = num1 + num2 + carry;
+		int sum = carry;
+
+		if (i >= 0)
+			sum += n1[i] - '0';
+
+		if (j >= 0)
+			sum += n2[j] - '0';
 
 		carry = sum / 10;
-		sum %= 10;
-		r[k] = sum + '0';
+		r[k] = (sum % 10) + '0';
 
 		i--;
 		j--;
 		k--;
 	}
+	if (k < 0 && (i >= 0 || j >= 0 || carry))
+		return (0);
 
-	return (k >= 0 ? r + k : 0);
+	return (r + k + 1);
 }
