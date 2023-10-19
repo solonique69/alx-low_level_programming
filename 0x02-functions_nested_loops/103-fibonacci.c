c9c56ad117edda89745e6e55f880569268a2a6d4
Solomon Okomowho <solonique2020@gmail.com>
2023-10-20 00:45:45 +0100

    Modified Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Computes the sum of even Fibonacci numbers
+ * not exceeding 4,000,000.
+ * Return: Alwyas 0 (Success)
+ */
+
+int main(void)
+{
+	int next, a = 1, b = 2, sum = 0;
+
+	while (a <= 4000000)
+	{
+		if (a % 2 == 0)
+		{
+			sum += a;
+		}
+
+		next = a + b;
+		a = b;
+		b = next;
+	}
+
+	printf("Sum of even Fibonacci numbers less than 4,000,000 : %d\n", sum);
+
+	return (0);
+}
