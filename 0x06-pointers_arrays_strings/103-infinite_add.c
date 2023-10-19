019068c5d4b981f397e9bfcfbf942db9699fe572
Solomon Okomowho <solonique2020@gmail.com>
2023-10-19 22:51:48 +0100

    Modified task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,42 +1,55 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * infinite_add - Adds two numbers
- * @n1: First number as a string
- * @n2: Second number as a string
- * @r: Buffer to store the result
- * @size_r: Size of the buffer
- * Return: Pointer to the result,
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = _strlen(n1) - 1;
-	int j = _strlen(n2) - 1;
-	int k = size_r - 2;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	r[size_r - 1] = '\0';
-
-	int carry = 0;
-
-	while (i >= 0 || j >= 0 || carry)
-	{
-		int sum = carry;
-
-		if (i >= 0)
-			sum += n1[i] - '0';
-
-		if (j >= 0)
-			sum += n2[j] - '0';
-
-		carry = sum / 10;
-		r[k] = (sum % 10) + '0';
-
-		i--;
-		j--;
-		k--;
-	}
-	if (k < 0 && (i >= 0 || j >= 0 || carry))
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-
-	return (r + k + 1);
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
+	{
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+			*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
+	}
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
