691d5d53082e05ff3f6ee7e52ffea5e64ff86054
Solomon Okomowho <solonique2020@gmail.com>
2023-10-20 01:01:01 +0100

    Modified Task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 98 Fibonacci numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+	unsigned long a = 1, b = 2, next;
+
+	printf("%lu, %lu, ", a, b);
+
+	for (n = 3; n <= 98; n++)
+	{
+		next = a + b;
+
+		if (n < 98)
+		{
+			printf("%lu, ", next);
+		}
+		else
+		{
+			printf("%lu\n", next);
+		}
+
+		a = b;
+		b = next;
+	}
+
+	return (0);
+}
