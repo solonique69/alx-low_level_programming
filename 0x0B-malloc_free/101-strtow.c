0f4f38ab3d1f5b6221bb5a4e0ee8cac39f10d61b
Solomon Okomowho <solonique2020@gmail.com>
2023-10-31 10:52:37 +0100

    file: Modified Task 6 - I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,13 +3,13 @@
 
 /**
  * argstostr - Concatenates all arguments of the program into a string;
- *             arguments are separated by a new line in the string.
+   arguments are separated by a new line in the string.
  * @ac: The number of arguments passed to the program.
  * @av: An array of pointers to the arguments.
- *
  * Return: If ac == 0, av == NULL, or the function fails - NULL.
- *         Otherwise - a pointer to the new string.
+ * Otherwise - a pointer to the new string.
  */
+
 char *argstostr(int ac, char **av)
 {
 	char *str;
