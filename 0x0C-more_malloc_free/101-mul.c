fa49ac910c55079298883d4ab461e550bf0df984
Solomon Okomowho <solonique2020@gmail.com>
2023-11-03 13:00:38 +0100

    file: Task 5 - modified

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,126 +1,66 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 
 /**
- * _is_zero - determines if any number is zero
- * @argv: argument vector.
+ * is_numeric - Check if a string contains only digits
+ * @str: The input string to check
  *
- * Return: no return.
+ * Return: 1 if all characters are digits, 0 otherwise
  */
-void _is_zero(char *argv[])
+
+int is_numeric(const char *str)
 {
-	int i, isn1 = 1, isn2 = 1;
-
-	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
-
-	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
-
-	if (isn1 == 1 || isn2 == 1)
+	while (*str)
 	{
-		printf("0\n");
-		exit(0);
-	}
-}
-
-/**
- * _initialize_array - set memery to zero in a new array
- * @ar: char array.
- * @lar: length of the char array.
- *
- * Return: pointer of a char array.
- */
-char *_initialize_array(char *ar, int lar)
-{
-	int i = 0;
-
-	for (i = 0; i < lar; i++)
-		ar[i] = '0';
-	ar[lar] = '\0';
-	return (ar);
-}
-
-/**
- * _checknum - determines length of the number
- * and checks if number is in base 10.
- * @argv: arguments vector.
- * @n: row of the array.
- *
- * Return: length of the number.
- */
-int _checknum(char *argv[], int n)
-{
-	int ln;
-
-	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
+		if (*str < '0' || *str > '9')
 		{
-			printf("Error\n");
-			exit(98);
+			return (0); /* Not a digit */
 		}
 
-	return (ln);
+		str++;
+	}
+
+	return (1); /* All characters are digits */
 }
 
 /**
- * main - Entry point.
- * program that multiplies two positive numbers.
- * @argc: number of arguments.
- * @argv: arguments vector.
+ * multiply - Multiply two positive numbers
+ * @num1: The first positive number
+ * @num2: The second positive number
  *
- * Return: 0 - success.
+ * Return: The product of num1 and num2
  */
+
+long long multiply(long long num1, long long num2)
+{
+	return (num1 * num2);
+}
+
 int main(int argc, char *argv[])
 {
-	int ln1, ln2, lnout, add, addl, i, j, k, ca;
-	char *nout;
-
 	if (argc != 3)
-		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
-	if (nout == NULL)
-		printf("Error\n"), exit(98);
-	nout = _initialize_array(nout, lnout);
-	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-	for (; k >= 0; k--, i--)
 	{
-		if (i < 0)
-		{
-			if (addl > 0)
-			{
-				add = (nout[k] - '0') + addl;
-				if (add > 9)
-					nout[k - 1] = (add / 10) + '0';
-				nout[k] = (add % 10) + '0';
-			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
-		}
-		if (j < 0)
-		{
-			if (nout[0] != '0')
-				break;
-			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-		}
-		if (j >= 0)
-		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = add / 10, nout[k] = (add % 10) + '0';
-		}
+		printf("Error\n");
+		return (98);
 	}
-	printf("%s\n", nout);
+
+	if (!is_numeric(argv[1]) || !is_numeric(argv[2]))
+	{
+		printf("Error\n");
+		return (98);
+	}
+	long long num1 = atoll(argv[1]);
+	long long num2 = atoll(argv[2]);
+
+	if (num1 <= 0 || num2 <= 0)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	long long result = multiply(num1, num2);
+
+	printf("%lld\n", result);
+
 	return (0);
 }
