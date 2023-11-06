dc4150bf162146a4edcb41498a2f7c28968e77bc
Solomon Okomowho <solonique2020@gmail.com>
2023-11-06 17:14:06 +0100

    Dir/file: Created dog.h task -0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
